cf2a6251ff3377489d433f3e7ca42e3cf743ef86
Your Name <you@example.com>
2019-01-27 12:54:36 +0530

    registration update

---
+++ accounts/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User,auth
+
+# Create your views here.
+
+def register(request):
+	if request.method == 'POST':
+		#get form values
+		first_name = request.POST['first_name']
+		last_name = request.POST['last_name']
+		email = request.POST['email']
+		username = request.POST['username']
+		password = request.POST['password']
+		password2 = request.POST['password2']
+
+		#validation
+		if password == password2:
+			if User.objects.filter(username=username).exists():
+				messages.error(request,'Username already exists')
+				return redirect('register')
+			else:
+				if User.objects.filter(email=email).exists():
+					messages.error(request,'email already exists')
+					return redirect('register')
+				else:
+					#looks good
+					user = User.objects.create_user(username=username,password=password,email=email,first_name=first_name,last_name=last_name)
+					#login after reg
+					#auth.login(request,user)
+					#messages.success(request,'register successfully ,now logged in')
+					#redirect('index')
+					user.save()
+					messages.success(request,'register successfully')
+					return redirect('login')
+		else:
+			messages.error(request,'Passwords do not match')
+			return redirect('register')
+	else:
+		return render(request, 'accounts/register.html')
+
+def login(request):
+	if request.method == 'POST':
+		print('submitted')
+		return redirect('login')
+	else:
+		return render(request, 'accounts/login.html')
+
+def logout(request):
+	return redirect('index')
+
+def dashboard(request):
+	return render(request, 'accounts/dashboard.html')
--- pages/views.py
+++ pages/views.py
@@ -7,7 +7,7 @@ from realtors.models import Realtor
 def index(request):
 	listings = Listing.objects.order_by('list_date').filter(is_published=True)[:3]
 	context = {
-	'listings':listings
+	'listings':listings,
 	}
 	return render(request,'pages/index.html',context)
 
